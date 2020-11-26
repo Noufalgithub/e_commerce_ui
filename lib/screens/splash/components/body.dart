89a41b8eafed13c8dadddff70a9fd9867a0b2d2c
noufal.github <noufal.github@gmail.com>
2020-11-26 11:33:33 +0700

    coba push

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,1 +1,70 @@
+import 'package:e_commerce_ui/constants.dart';
+import 'package:e_commerce_ui/size_config.dart';
 import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: SplashContent(
+                text: "Welcome to TOKOpal, let's shop!",
+                image: "assets/images/splash_1.png",
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+        Text(
+          "TOKOpal",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidht(36),
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(text),
+        Spacer(
+          flex: 2,
+        ),
+        Image.asset(
+          image,
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenWidht(235),
+        )
+      ],
+    );
+  }
+}
